441507ca9aa1f1e518d182cafd516b597ae163db
maximthebigman <84477054+maximthebigman@users.noreply.github.com>
2021-08-13 14:58:28 +0100

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,11 @@
+class Paper{
+    constructor(){
+        var options={
+            isStatic:falsse,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+        
+    }
+}
